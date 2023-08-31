59fc720796f1250c77c59975bdf4a7eaf0ce87d7
MaggySanchez <129992475+MaggySanchez@users.noreply.github.com>
2023-08-31 14:27:05 -0300

    Volviendo a version inicial del proyecto

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        agregarUsuario();
-    }
-
-    public static void agregarUsuario() {
-        Scanner scanner = new Scanner(System.in);
-        List<Usuario> usuarios = new ArrayList<>();
-        boolean repetir = true;
-        do {
-            System.out.println("Ingrese su nombre de usuario");
-            String nuevoNombre = scanner.nextLine();
-            System.out.println("Ingrese su clave");
-            String nuevaCLave = scanner.nextLine();
-            System.out.println("Ingrese su email");
-            String nuevoEmail = scanner.nextLine();
-            Usuario nuevoUsuario = new Usuario(nuevoNombre, nuevaCLave, nuevoEmail);
-            usuarios.add(nuevoUsuario);
-            System.out.println("¿Desea agregar un nuevo usuario?");
-            String respuesta = scanner.nextLine();
-            if (respuesta.equalsIgnoreCase("NO")) {
-                repetir = false;
-            }
-        } while (repetir);
-        System.out.println(usuarios);
     }
 }
